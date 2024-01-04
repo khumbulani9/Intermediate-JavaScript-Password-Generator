6515d88fe8383c000a9d62be3d6f8b6ca8ff7be8
khumbulani9 <mduduzi1234gmail.com>
2024-01-04 01:54:00 +0000

    update

--- starter/script.js
+++ starter/script.js
@@ -1,118 +1,82 @@
-// Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+// Assignment code here
+var length;
+var number;
+var specialCharacter;
+var upperCase;
+var lowerCase;
+var selection;
+var lettersUp
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const characters = ["number", "letters", "character"];
 
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
-function generatePassword() {
-
-}
-
+number = "0123456789";
+letters = "abcdefghijklmnopqrstuvwxyz";
+lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+character = "!@#$%^&*)()";
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
-
+var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
+// Begin setting password parameters
+function generatePassword() {
+  // Select # of characters for new password
+  length = prompt("Enter the number of characters you'd like for your new password (Select a number between 8-128)");
+  if (!length) {
+    alert("Please make a valid entry");
+  } else if (length < 8 || length > 128) {
+    // Function recognizes incorrect input
+    length = prompt("New password must be from 8-128 characters");
+  } else {
+    // Prompts continue when length input is valid
+    number = confirm("Include numbers in your new password?");
+    specialCharacter = confirm("Include special characters in your new password?");
+    upperCase = confirm("Include uppercase letters?");
+    lowerCase = confirm("Include lowercase letters?");
+  };
+  // for all of the above true
+  if (number && specialCharacter && upperCase && lowerCase) {
+    selection = character.concat(number, letters, lettersUp);
+  }
+  // three options selected
+  else if (number && specialCharacter && upperCase) {
+    selection = character.concat(number, lettersUp);
+  } else if (specialCharacter && upperCase && lowerCase) {
+    selection = character.concat(letters, lettersUp);
+  } else if (upperCase && lowerCase && number) {
+    selection = lettersUp.concat(letters, number);
+  } else if (lowerCase && number && specialCharacter) {
+    selection = letters.concat(number, character);
+  }
+  //two options selected
+  else if (specialCharacter && number) {
+    selection = character.concat(number);
+  } else if (number && upperCase) {
+    selection = number.concat(lettersUp);
+  } else if (upperCase && lowerCase) {
+    selection = lettersUp.concat(letters);
+  } else if (lowerCase && number) {
+    selection = letters.concat(number);
+  } else if (lowerCase && specialCharacter) {
+    selection = letters.concat(character);
+  } else if (specialCharacter && upperCase) {
+    selection = character.concat(lettersUp);
+  }
+  //one option
+  else if (specialCharacter) {
+    selection = character;
+  } else if (number) {
+    selection = number;
+  } else if (lowerCase) {
+    selection = letters;
+  } else if (upperCase) {
+    selection = lettersUp;
+  };
+  return selection;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
